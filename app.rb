0ae51c6ac77c328d2d099381de91fb34c5983288
Calum Darroch <calum_darroch@hotmail.com>
2019-04-16 12:53:53 +0100

    First commit: Running Sinatra, Shotgun, Thin. Working with HTML.

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+set :session_secret, 'super secret'
+
+get '/cat' do
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+    </div>"
+end
+
+get '/' do
+  'Hello World!'
+end
+
+get '/secret' do
+  'The best jam is loganberry'
+end
+
+get '/ultrasecret' do
+  'idkfa'
+end
